168d5c59d82bb57d4f25e68bba221035f77e2ea9
Andrey <goncharenkoandrey23@ukr.net>
2021-01-30 19:28:03 +0200

    resize complete

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import {ExcelComponent} from "@core/ExcelComponent";
-import {$} from "@core/dom";
-import {createTable} from "./table.template.js";
+import {createTable} from "./table.template";
+import {shouldResize} from "./table.function";
+import {resizeHandler} from "./table.resize";
 export class Table extends ExcelComponent {
 	static className = "excel__table";
 	constructor($root) {
@@ -14,47 +15,8 @@ export class Table extends ExcelComponent {
 	onClick() {
 	}
 	onMousedown() {
-		if(event.target.dataset.resize) {
-			const $resizer = $(event.target);
-			const $parent = $resizer.closest('[data-type="resizable"]');
-			const coordinates = $parent.getCoordinates();
-			const type = $resizer.data.resize;
-			const sideProperty = type === "column" ? "bottom" : "right";
-			let value;
-			$resizer.css({
-				opacity: 1,
-				[sideProperty]: "-5000px"
-			});
-			document.onmousemove = (e) => {
-				if(type === "column") {
-					const delta = e.pageX - coordinates.right;
-					value = coordinates.width + delta;
-					$resizer.css({right: -delta + "px"});
-				}
-				else {
-					const delta = e.pageY - coordinates.bottom;
-					value = coordinates.height + delta;
-					$resizer.css({
-						bottom: -delta + "px"
-					});
-				}
-			};
-			document.onmouseup = () => {
-				document.onmousemove = null;
-				document.onmouseup = null;
-				if(type === "column") {
-					$parent.css({width: value + "px"});
-					this.$root.findAll(`[data-column="${$parent.data.column}"]`).forEach((element) => element.style.width = value + "px");
-				}
-				else {
-					$parent.css({height: value + "px"});
-				}
-				$resizer.css({
-					opacity: 0,
-					bottom: 0,
-					right: 0
-				});
-			};
+		if(shouldResize(event)) {
+			resizeHandler(this.$root, event);
 		}
 	}
 	onMouseup() {
