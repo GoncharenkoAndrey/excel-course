63cc8c30e6a7317dfadcd40a7ffee4d656ca28f0
Andrey <goncharenkoandrey23@ukr.net>
2021-01-30 11:24:50 +0200

    column resize logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,41 +1,35 @@
 import {ExcelComponent} from "@core/ExcelComponent";
+import {$} from "@core/dom";
+import {createTable} from "./table.template.js";
 export class Table extends ExcelComponent {
 	static className = "excel__table";
+	constructor($root) {
+		super($root, {
+			listeners: ["mousedown"]
+		});
+	}
 	toHTML() {
-		return `
-					<div class="row">
-						<div class="row-info">
-						</div>
-						<div class="row-data">
-							<div class="column">
-								A
-							</div>
-							<div class="column">
-								B
-							</div>
-							<div class="column">
-								C
-							</div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="row-info">
-						</div>
-						<div class="row-data">
-							<div class="cell selected" contenteditable>1</div>
-							<div class="cell">2</div>
-							<div class="cell">3</div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="row-info">
-						</div>
-						<div class="row-data">
-							<div class="cell selected">1</div>
-							<div class="cell">2</div>
-							<div class="cell">3</div>
-						</div>
-					</div>
-		`;
+		return createTable(20);
+	}
+	onClick() {
+	}
+	onMousedown() {
+		if(event.target.dataset.resize) {
+			const $resizer = event.target;
+			const $parent = $($resizer.closest('[data-type="resizable"]'));
+			const coordinates = $parent.getCoordinates();
+			document.onmousemove = (e) => {
+				const delta = e.pageX - coordinates.right;
+				const value = coordinates.width + delta;
+				$parent.$el.style.width = value + "px";
+			};
+			document.onmouseup = () => {
+				document.onmousemove = null;
+			};
+		}
+	}
+	onMouseup() {
+	}
+	onMousemove() {
 	}
 }
---
+++ src/components/table/table.template.js
@@ -0,0 +1,42 @@
+const CODES = {
+	A: 65,
+	Z: 90
+};
+function toCell() {
+	return `<div class="cell" contenteditable></div>`;
+}
+function toColumn(column) {
+	return `<div class="column" data-type="resizable">
+		${column}
+		<div class="column-resize" data-resize="column"></div>
+	</div>`;
+}
+function createRow(index, content) {
+	const resize = index ? '<div class="row-resize" data-resize="row"></div>' : "";
+	return `<div class="row">
+		<div class="row-info">${index ? index : ''}</div>
+		${resize}
+		<div class="row-data">${content}</div>
+		
+	</div>`;
+}
+function toChar(element, index) {
+	return String.fromCharCode(CODES.A + index);
+}
+export function createTable(rowsCount = 15) {
+	const columnsCount = CODES.Z - CODES.A + 1;
+	const rows = [];
+	const columns = new Array(columnsCount).fill('')
+		.map(toChar)
+		.map(toColumn)
+		.join('');
+	rows.push(createRow(null, columns));
+	for(let i = 0; i < rowsCount; i++) {
+		const cells = new Array(columnsCount)
+			.fill('')
+			.map(toCell)
+			.join('');
+		rows.push(createRow(i + 1, cells));
+	}
+	return rows.join('');
+}
